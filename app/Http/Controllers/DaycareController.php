de24cb45e430252e8b74d74cede333fd30bcb098
Satiko <satikoshapy@yahoo.com>
2022-01-31 11:57:24 +0100

    delete method added to parents

--- app/Http/Controllers/DaycareController.php
+++ app/Http/Controllers/DaycareController.php
@@ -97,5 +97,11 @@ class DaycareController extends Controller
         return response()->json($post, 201);
     }
 
+    public function deleteParent($id)
+     {
+        Parents::findOrFail($id)->delete();
+        return response('Deleted succesfully', 200);        
+     }
+
 
 }
